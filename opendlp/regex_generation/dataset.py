fc0ace1a8f615f5563e518f2bf4bc3cc4e13eb04
Longice <1337913069@qq.com>
2022-07-31 18:37:03 +0800

    implement dataset

--- opendlp/regex_generation/dataset.py
+++ opendlp/regex_generation/dataset.py
@@ -1,16 +1,13 @@
-import os
+import re
 import pandas as pd
-
-from opendlp_server import LOGGER
 
 class Dataset:
     def __init__(self, data_path):
         self.data_path = data_path
 
-
     def clean(self, strings):
         res = []
-        tmp = [s.strip() for s in strings]
+        tmp = [s.strip().replace(' ', '') for s in strings]
         for s in tmp:
             if s != '':
                 res.append(s)
@@ -28,6 +25,13 @@ class Dataset:
         pos_examples, neg_examples = self.read_csv_data()
         self.__pos_examples = pos_examples
         self.__neg_examples = neg_examples
+        self.is_fixed_length = self.same_length()
+
+    def same_length(self):
+        for i in range(len(self.pos_examples) - 1):
+            if len(self.pos_examples[i + 1]) != len(self.pos_examples[i]):
+                return False
+        return True
 
     @property
     def pos_examples(self):
@@ -39,8 +43,23 @@ class Dataset:
 
     def remove_by_regex(self, regex_tree):
         """
-        remove positive samples that can be mathced by regex_tree
+        remove positive samples that can be matched by regex_tree
         @param regex_tree: a regex tree
         @return: None
         """
-        raise NotImplementedError
+        flags = re.DOTALL | re.MULTILINE
+        pattern = regex_tree.form(string='')
+        matched_ids = set()
+        for i in range(len(self.__pos_examples)):
+            example = self.__pos_examples[i]
+            matches = re.finditer(pattern, example, flags=flags)
+            for match in matches:
+                if match.span()[1]-match.span()[0] == len(example):
+                    matched_ids.add(i)
+
+        pos_examples = []
+        for i in range(len(self.__pos_examples)):
+            if i not in matched_ids:
+                pos_examples.append(self.__pos_examples[i])
+        self.__pos_examples = pos_examples
+

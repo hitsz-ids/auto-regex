3ad3a208083429fda0585d690923d11f20d21e0b
Longice <12068001+Longice@users.noreply.github.com>
2022-08-19 20:39:59 +0800

    bug fix

--- service/opendlp_server.py
+++ service/opendlp_server.py
@@ -33,12 +33,14 @@ class DLPServer(sensitive_pb2_grpc.OpenDlpServiceServicer):
         status = sensitive_pb2.Status()
         status.code = sensitive_pb2.OK
         status = check_param_sensitive(status, to_analyze_file_path, user_define_pattern_file, thresholds)
-        if thresholds != '':
-            thresholds = json.loads(thresholds)
-        else:
-            thresholds = dict()
+
         result = {}
         if status.code == sensitive_pb2.OK:
+            if thresholds != '':
+                thresholds = json.loads(thresholds)
+            else:
+                thresholds = dict()
+
             try:
                 result = table_analyzer.analyze(to_analyze_file_path, user_define_pattern_file, thresholds)
             except exceptions.FILE_READ_ERROR as error:
